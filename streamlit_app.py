209073309405562f99cd50426979f7e4ee2a1890
manueldavidc <manuel.castillo@correlation-one.com>
2024-09-09 13:06:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from scraper import fetch_html_selenium, save_raw_data, format_data, save_format
 
 
 # Initialize Streamlit app
-st.set_page_config(page_title="Universal Web Scraper")
+st.set_page_config(page_title="Eyes Everywhere")
 st.title("Universal Web Scraper 🦑")
 
 # Sidebar components
