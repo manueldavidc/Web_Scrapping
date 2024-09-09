a838d98792a511f3ea0d3568fa13243ff0aec574
manueldavidc <manuel.castillo@correlation-one.com>
2024-09-09 13:56:41 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,27 +16,28 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from openai import OpenAI
 
 load_dotenv()
 
 # Set up the Chrome WebDriver options
 
+
+
 def setup_selenium():
     options = Options()
-
-    # adding arguments
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    
-    # Randomize user-agent to mimic different users
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    # Specify the path to the ChromeDriver
-    service = Service(r"./chromedriver-win64/chromedriver.exe")  
 
-    # Initialize the WebDriver
+    # Get ChromeDriver path from environment variable
+    chrome_driver_path = os.getenv('CHROMEDRIVER_PATH', './chromedriver')
+
+    service = Service(chrome_driver_path)
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
